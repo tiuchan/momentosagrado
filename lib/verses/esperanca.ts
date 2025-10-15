8f4bdc97acf1d2158fa2d06cb00048cd9bbafeb1
tiuchan <tiuchan@gmail.com>
2025-10-15 17:16:37 -0300

    Update esperanca.ts

--- lib/verses/esperanca.ts
+++ lib/verses/esperanca.ts
@@ -13,4 +13,149 @@ export const esperancaVerses: Verse[] = [
     ref: "Salmos 42:5",
     text: "Por que você está assim tão triste, ó minha alma? Por que está assim tão perturbada dentro de mim? Ponha a sua esperança em Deus!",
   },
+
+  {
+    ref: "Hebreus 11:1",
+    text: "Ora, a fé é a certeza daquilo que esperamos e a prova das coisas que não vemos.",
+  },
+  {
+    ref: "Romanos 8:24-25",
+    text: "Pois nessa esperança fomos salvos. Contudo, esperança que se vê não é esperança, pois quem espera por aquilo que está vendo? Entretanto, se esperamos o que não vemos, com paciência o aguardamos.",
+  },
+  {
+    ref: "Hebreus 10:23",
+    text: "Apeguemo-nos com firmeza à esperança que professamos, pois aquele que prometeu é fiel.",
+  },
+  {
+    ref: "1 Timóteo 4:10",
+    text: "Se trabalhamos e lutamos é porque temos colocado a nossa esperança no Deus vivo, o Salvador de todos os homens, especialmente dos que creem.",
+  },
+  {
+    ref: "Colossenses 1:27",
+    text: "A eles quis Deus dar a conhecer entre os gentios a gloriosa riqueza deste mistério, que é Cristo em vocês, a esperança da glória.",
+  },
+  {
+    ref: "Salmos 31:24",
+    text: "Sejam fortes e corajosos, todos vocês que esperam no Senhor!",
+  },
+  {
+    ref: "Salmos 146:5",
+    text: "Como é feliz aquele cujo auxílio é o Deus de Jacó, cuja esperança está no Senhor, no seu Deus!",
+  },
+  {
+    ref: "Salmos 25:2-3",
+    text: "Em ti confio, ó meu Deus. Não deixes que eu seja humilhado, nem que os meus inimigos triunfem sobre mim! Nenhum dos que esperam em ti ficará decepcionado; decepcionados ficarão aqueles que, sem motivo, agem traiçoeiramente.",
+  },
+  {
+    ref: "Isaías 40:31",
+    text: "Mas aqueles que esperam no Senhor renovam as suas forças. Voam bem alto como águias; correm e não ficam exaustos, andam e não se cansam.",
+  },
+  {
+    ref: "Lamentações 3:21-24",
+    text: "Todavia, lembro-me também do que me pode dar esperança: Graças ao grande amor do Senhor é que não somos consumidos, pois as suas misericórdias são inesgotáveis. Renovam-se cada manhã; grande é a sua fidelidade! Digo a mim mesmo: 'A minha porção é o Senhor; portanto, nele porei a minha esperança.'",
+  },
+  {
+    ref: "Salmos 33:20-22",
+    text: "A nossa esperança está no Senhor; ele é o nosso auxílio e o nosso escudo. Nele se alegra o nosso coração, pois confiamos no seu santo nome. Esteja sobre nós o teu amor leal, Senhor, como em ti está a nossa esperança.",
+  },
+  {
+    ref: "Romanos 12:12",
+    text: "Alegrem-se na esperança, sejam pacientes na tribulação, perseverem na oração.",
+  },
+  {
+    ref: "1 Pedro 3:15",
+    text: "Antes, santifiquem Cristo como Senhor no coração. Estejam sempre preparados para responder a qualquer pessoa que lhes pedir a razão da esperança que há em vocês.",
+  },
+  {
+    ref: "Salmos 62:5",
+    text: "Descanse somente em Deus, ó minha alma; dele vem a minha esperança.",
+  },
+  {
+    ref: "Efésios 1:18",
+    text: "Oro também para que os olhos do coração de vocês sejam iluminados, a fim de que vocês conheçam a esperança para a qual ele os chamou, as riquezas da gloriosa herança dele nos santos.",
+  },
+  {
+    ref: "2 Coríntios 3:12",
+    text: "Portanto, tendo tal esperança, usamos grande franqueza de discurso.",
+  },
+  {
+    ref: "1 Tessalonicenses 1:3",
+    text: "Lembramos continuamente, diante de nosso Deus e Pai, o que vocês têm demonstrado: o trabalho que resulta da fé, o esforço motivado pelo amor e a perseverança proveniente da esperança em nosso Senhor Jesus Cristo.",
+  },
+  {
+    ref: "Salmos 119:114",
+    text: "Tu és o meu abrigo e o meu escudo; e na tua palavra coloquei a minha esperança.",
+  },
+  {
+    ref: "Judas 1:21",
+    text: "Mantenham-se no amor de Deus, enquanto esperam a misericórdia de nosso Senhor Jesus Cristo para a vida eterna.",
+  },
+  {
+    ref: "Isaías 30:18",
+    text: "Contudo, o Senhor espera o momento de ser bondoso com vocês; ele ainda se levantará para mostrar-lhes compaixão. Pois o Senhor é Deus de justiça. Como são felizes todos os que nele esperam!",
+  },
+  {
+    ref: "Filipenses 3:20",
+    text: "A nossa cidadania, porém, está nos céus, de onde esperamos ansiosamente um Salvador, o Senhor Jesus Cristo.",
+  },
+  {
+    ref: "Romanos 5:3-4",
+    text: "Não só isso, mas também nos gloriamos nas tribulações, porque sabemos que a tribulação produz perseverança; a perseverança, um caráter aprovado; e o caráter aprovado, esperança.",
+  },
+  {
+    ref: "Romanos 15:4",
+    text: "Pois tudo o que foi escrito no passado foi escrito para nos ensinar, de forma que, por meio da perseverança e do bom ânimo procedentes das Escrituras, mantenhamos a nossa esperança.",
+  },
+  {
+    ref: "Salmos 39:7",
+    text: "Mas agora, Senhor, que hei de esperar? Minha esperança está em ti.",
+  },
+  {
+    ref: "Isaías 8:17",
+    text: "Esperarei pelo Senhor, que está escondendo o seu rosto da descendência de Jacó. Nele porei a minha esperança.",
+  },
+  {
+    ref: "Salmos 5:3",
+    text: "De manhã ouves, Senhor, o meu clamor; de manhã te apresento a minha oração e aguardo com esperança.",
+  },
+  {
+    ref: "Salmos 33:22",
+    text: "Esteja sobre nós o teu amor, Senhor, como está em ti a nossa esperança.",
+  },
+  {
+    ref: "Provérbios 20:22",
+    text: "Não diga: ‘Eu o farei pagar pelo mal que me fez!’ Espere pelo Senhor, e ele dará a vitória a você.",
+  },
+  {
+    ref: "Salmos 37:9",
+    text: "Pois os maus serão eliminados, mas os que esperam no Senhor receberão a terra por herança.",
+  },
+  {
+    ref: "Salmos 37:34",
+    text: "Espere no Senhor e siga a sua vontade. Ele o exaltará, dando-lhe a terra por herança.",
+  },
+  {
+    ref: "Salmos 38:15",
+    text: "Senhor, em ti espero; Tu me responderás, ó Senhor meu Deus!",
+  },
+  {
+    ref: "Salmos 27:14",
+    text: "Espere no Senhor. Seja forte! Coragem! Espere no Senhor.",
+  },
+  {
+    ref: "Salmos 40:1",
+    text: "Coloquei toda minha esperança no Senhor; ele se inclinou para mim e ouviu o meu clamor de socorro.",
+  },
+  {
+    ref: "Salmos 130:5-6",
+    text: "Espero no Senhor com todo o meu ser, e na sua palavra ponho a minha esperança. Espero pelo Senhor mais do que as sentinelas pela manhã.",
+  },
+  {
+    ref: "Salmos 25:3",
+    text: "Nenhum dos que esperam em ti ficará decepcionado; decepcionados ficarão aqueles que, sem motivo, agem traiçoeiramente.",
+  },
+  {
+    ref: "Salmos 62:5",
+    text: "Descanse somente em Deus, ó minha alma; dele vem a minha esperança.",
+  },
 ]
